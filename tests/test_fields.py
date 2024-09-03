8e677865ca6e47c73e433a827d20340d5e5fcfcb
Dave Currie <dcurrie@thelabnyc.com>
2024-09-03 12:04:52 -0400

    fix: fix handling of v3 required score when set to 0
    Fixes two closely related bugs:
    
    1. Previously if the required score was set to 0 (in an attempt to not i
       actually block anything), the default of 0.5 would be used instead. This was
       due to incorrect logic when checking for None values. This changes fixes this
       handling so that a requred score of 0 may be set.
    2. The docs state that "Validation is passed if the score value returned by
       Google is greater than or equal to required score." However the code was
       actually checking for a score greater tahan required, not greater than or
       equal to. This change fixes the logic to match the stated behavior.

--- tests/test_fields.py
+++ tests/test_fields.py
@@ -1,5 +1,6 @@
 import pytest
 from django.core.exceptions import ImproperlyConfigured
+from drf_recaptcha import constants
 
 from drf_recaptcha.fields import ReCaptchaV2Field, ReCaptchaV3Field
 from drf_recaptcha.validators import ReCaptchaV2Validator, ReCaptchaV3Validator
@@ -118,3 +119,22 @@ def test_recaptcha_v3_field_score_improperly_configured(
         ReCaptchaV3Field(action="test_action", **params)
 
     assert str(exc_info.value) == error
+
+
+@pytest.mark.parametrize(
+    ("params", "from_settings", "settings_default", "expected"),
+    [
+        ({"required_score": 0}, {}, None, 0),  # Expect 0 because it's explicitly set
+    ],
+)
+def test_recaptcha_v3_field_valid_scores(
+    params, from_settings, settings_default, expected, settings, monkeypatch
+):
+    # Mock settings
+    monkeypatch.setattr(constants, "DEFAULT_V3_SCORE", None)
+
+    settings.DRF_RECAPTCHA_ACTION_V3_SCORES = from_settings
+    settings.DRF_RECAPTCHA_DEFAULT_V3_SCORE = settings_default
+
+    field = ReCaptchaV3Field(action="test_action", **params)
+    assert field.required_score == expected
